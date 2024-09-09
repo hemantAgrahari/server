d1ba02e28d0ab267147b13595722b484cb0c129d
Hemant <Hemant11102219@gmail.com>
2024-09-09 20:26:46 +0530

    cors issue

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors());
+
+app.use(cors({ origin: 'https://server-tau-navy.vercel.app/' }));
 app.use(cookieParser());
 app.use(express.json());
 
